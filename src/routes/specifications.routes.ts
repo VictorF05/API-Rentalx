b3a48b6beb96f771356529b12a3009b34671c760
Víctor Franco <victorfranco5@outlook.com>
2022-09-24 20:04:10 -0300

    singleton pattern for class repositories

--- src/routes/specifications.routes.ts
+++ src/routes/specifications.routes.ts
@@ -1,22 +1,11 @@
 import { Router } from "express";
 
-import { SpecificationsRepository } from "../modules/cars/repositories/SpecificationsRepository";
-import { CreateSpecificationUseCase } from "../modules/cars/useCases/createSpecification/CreateSpecificationUseCase";
+import { createSpecificationController } from "../modules/cars/useCases/createSpecification";
 
 const specificationsRoutes = Router();
 
-const specifcationsRepository = new SpecificationsRepository();
-
 specificationsRoutes.post("/", (request, response) => {
-    const { name, description } = request.body;
-
-    const createSpecificationUseCase = new CreateSpecificationUseCase(
-        specifcationsRepository
-    );
-
-    createSpecificationUseCase.execute({ name, description });
-
-    return response.status(201).send();
+    return createSpecificationController.handle(request, response);
 });
 
 export { specificationsRoutes };
